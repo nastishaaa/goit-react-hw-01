5b6e17dcd236445277efa1634ea03b29c136b3ad
Anastasiia <anastasiiprfnk@gmail.com>
2025-02-17 23:58:48 +0200

    task 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import userData from "./userData.json";
 import Profile from './components/Profile';
 import friends from './friends.json';
 import FriendList from './components/FriendList';
+import transactions from './transactions.json';
+import TransactionHistory from './components/TransactionHistory';
 
 
 function App() {
 
   return (
     <>
-    <h1>Profile</h1>
+    
       <Profile username={userData.username}
         tag={userData.tag}
         location={userData.location}
         avatar={userData.avatar}
         stats={userData.stats}
         />
-        <h1>Friend List</h1>
+        
         <FriendList friends={friends}/>
+        <TransactionHistory items={transactions} />
     </>
   )
 }
